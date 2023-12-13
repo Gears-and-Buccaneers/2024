baf7b996748d32db05952110b4b0bc4a3669362b
Sam Duckett <sduckett@dawsonstudents.org>
2023-12-12 21:49:43 -0700

    testing ci

--- src/test/java/IntakeUTest.java
+++ src/test/java/IntakeUTest.java
@@ -11,50 +11,49 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 class IntakeUTest {
-  static final double DELTA = 1e-2; // acceptable deviation range
-  Intake m_intake;
-  PWMSim m_simMotor;
-  DoubleSolenoidSim m_simPiston;
+    static final double DELTA = 1e-2; // acceptable deviation range
+    Intake m_intake;
+    PWMSim m_simMotor;
+    DoubleSolenoidSim m_simPiston;
 
-  @BeforeEach // this method will run before each test
-  void setup() {
-    assert HAL.initialize(500, 0); // initialize the HAL, crash if failed
-    m_intake = new Intake(); // create our intake
-    m_simMotor = new PWMSim(1); // create our simulation PWM motor controller
-    m_simPiston =
-        new DoubleSolenoidSim(PneumaticsModuleType.CTREPCM, 2, 3); // create our simulation solenoid
-  }
+    @BeforeEach // this method will run before each test
+    void setup() {
+        assert HAL.initialize(500, 0); // initialize the HAL, crash if failed
+        m_intake = new Intake(); // create our intake
+        m_simMotor = new PWMSim(10); // create our simulation PWM motor controller
+        m_simPiston = new DoubleSolenoidSim(PneumaticsModuleType.CTREPCM, 2, 3); // create our simulation solenoid
+    }
 
-  @SuppressWarnings("PMD.SignatureDeclareThrowsException")
-  @AfterEach // this method will run after each test
-  void shutdown() throws Exception {
-    m_intake.close(); // destroy our intake object
-  }
+    @SuppressWarnings("PMD.SignatureDeclareThrowsException")
+    @AfterEach // this method will run after each test
+    void shutdown() throws Exception {
+        m_intake.close(); // destroy our intake object
+    }
 
-  @Test // marks this method as a test
-  void doesntWorkWhenClosed() {
-    m_intake.retract(); // close the intake
-    m_intake.activate(0.5); // try to activate the motor
-    assertEquals(
-        0.0, m_simMotor.getSpeed(), DELTA); // make sure that the value set to the motor is 0
-  }
+    @Test // marks this method as a test
+    void doesntWorkWhenClosed() {
+        m_intake.retract(); // close the intake
+        m_intake.activate(0.5); // try to activate the motor
+        assertEquals(
+                0.0, m_simMotor.getSpeed(), DELTA); // make sure that the value set to the motor is 0
+    }
 
-  @Test
-  void worksWhenOpen() {
-    m_intake.deploy();
-    m_intake.activate(0.5);
-    assertEquals(0.5, m_simMotor.getSpeed(), DELTA);
-  }
+    @Test
+    void worksWhenOpen() {
+        m_intake.deploy();
+        m_intake.activate(0.5);
+        assertEquals(0.5, m_simMotor.getSpeed(), DELTA);
+    }
 
-  @Test
-  void retractTest() {
-    m_intake.retract();
-    assertEquals(DoubleSolenoid.Value.kReverse, m_simPiston.get());
-  }
+    @Test
+    void retractTest() {
+        m_intake.retract();
+        assertEquals(DoubleSolenoid.Value.kReverse, m_simPiston.get());
+    }
 
-  @Test
-  void deployTest() {
-    m_intake.deploy();
-    assertEquals(DoubleSolenoid.Value.kForward, m_simPiston.get());
-  }
+    @Test
+    void deployTest() {
+        m_intake.deploy();
+        assertEquals(DoubleSolenoid.Value.kForward, m_simPiston.get());
+    }
 }
