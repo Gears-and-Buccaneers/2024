a33b85d5c522f529306ce6343717ebbbe156bf94
Sam Duckett <sduckett@dawsonstudents.org>
2023-12-12 21:39:01 -0700

    things

--- src/main/java/frc/robot/Subsytems/Intake.java
+++ src/main/java/frc/robot/Subsytems/Intake.java
@@ -5,41 +5,38 @@ import edu.wpi.first.wpilibj.PneumaticsModuleType;
 import edu.wpi.first.wpilibj.motorcontrol.PWMSparkMax;
 
 public class Intake implements AutoCloseable {
-    private final PWMSparkMax m_motor;
-    private final DoubleSolenoid m_piston;
+  private final PWMSparkMax m_motor;
+  private final DoubleSolenoid m_piston;
 
-    public Intake() {
-        m_motor = new PWMSparkMax(1);
-        m_piston = new DoubleSolenoid(
-                PneumaticsModuleType.CTREPCM,
-                2,
-                3);
-    }
+  public Intake() {
+    m_motor = new PWMSparkMax(1);
+    m_piston = new DoubleSolenoid(PneumaticsModuleType.CTREPCM, 2, 3);
+  }
 
-    public void deploy() {
-        m_piston.set(DoubleSolenoid.Value.kReverse);
-    }
+  public void deploy() {
+    m_piston.set(DoubleSolenoid.Value.kForward);
+  }
 
-    public void retract() {
-        m_piston.set(DoubleSolenoid.Value.kReverse);
-        m_motor.set(0); // turn off the motor
-    }
+  public void retract() {
+    m_piston.set(DoubleSolenoid.Value.kReverse);
+    m_motor.set(0); // turn off the motor
+  }
 
-    public void activate(double speed) {
-        if (isDeployed()) {
-            m_motor.set(speed);
-        } else { // if piston isn't open, do nothing
-            m_motor.set(0);
-        }
+  public void activate(double speed) {
+    if (isDeployed()) {
+      m_motor.set(speed);
+    } else { // if piston isn't open, do nothing
+      m_motor.set(0);
     }
+  }
 
-    public boolean isDeployed() {
-        return m_piston.get() == DoubleSolenoid.Value.kForward;
-    }
+  public boolean isDeployed() {
+    return m_piston.get() == DoubleSolenoid.Value.kForward;
+  }
 
-    @Override
-    public void close() throws Exception {
-        m_piston.close();
-        m_motor.close();
-    }
+  @Override
+  public void close() throws Exception {
+    m_piston.close();
+    m_motor.close();
+  }
 }
