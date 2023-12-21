1395be865a35ea7e97565a689f3268123039972f
Sam Duckett <67984574+samduckett@users.noreply.github.com>
2023-12-21 00:22:12 -0700

    Delete javadoc directory

--- javadoc/package-search-index.js
+++
@@ -1,1 +0,0 @@
-packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"frc.lib.hardware.joystick"},{"l":"frc.lib.hardware.motorController"},{"l":"frc.lib.hardware.motorController.Sim"},{"l":"frc.robot"},{"l":"frc.robot.joystics"},{"l":"frc.robot.Subsytems.Intake"}];updateSearchResults();
