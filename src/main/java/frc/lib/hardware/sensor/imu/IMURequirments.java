370627b747a99374852a4d3ca20e45d14b27cbf5
Sam Duckett <sduckett@dawsonstudents.org>
2023-12-22 15:30:35 -0700

    adding Pigeon to lib

--- src/main/java/frc/lib/hardware/sensor/imu/IMURequirments.java
+++ src/main/java/frc/lib/hardware/sensor/imu/IMURequirments.java
@@ -1,5 +1,17 @@
 package frc.lib.hardware.sensor.imu;
 
+import edu.wpi.first.math.geometry.Rotation3d;
+
+/**
+ * refer to https://docs.wpilib.org/en/latest/docs/hardware/sensors/proximity-switches.html for more infomation
+ */
 public interface IMURequirments {
-    
+    double getPitch();
+    double getYaw();
+    double getRoll();
+
+
+    public default Rotation3d getRotation3d() {
+        return new Rotation3d(getRoll(), getPitch(), getYaw());
+    }
 }
---
+++ src/main/java/frc/lib/hardware/sensor/imu/Pigeon.java
@@ -0,0 +1,26 @@
+package frc.lib.hardware.sensor.imu;
+
+import com.ctre.phoenix6.hardware.Pigeon2;
+
+public class Pigeon implements IMURequirments {
+    private Pigeon2 pigeon2;
+
+    public Pigeon() {
+        pigeon2 = new Pigeon2(0);
+    }
+
+    @Override
+    public double getPitch() {
+        return pigeon2.getPitch().getValueAsDouble();
+    }
+
+    @Override
+    public double getYaw() {
+        return pigeon2.getYaw().getValueAsDouble();
+    }
+
+    @Override
+    public double getRoll() {
+        return pigeon2.getRoll().getValueAsDouble();
+    }
+}
