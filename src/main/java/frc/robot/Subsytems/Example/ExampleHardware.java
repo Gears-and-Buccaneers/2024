913817ff6f4234cc1fb1b2e9dd7e4cb8b917fba0
Sam Duckett <sduckett@dawsonstudents.org>
2023-12-27 19:59:51 -0700

    a

--- src/main/java/frc/robot/Subsytems/Example/ExampleHardware.java
+++ src/main/java/frc/robot/Subsytems/Example/ExampleHardware.java
@@ -1,10 +1,24 @@
 package frc.robot.Subsytems.Example;
 
+import org.littletonrobotics.junction.LogTable;
+
 public class ExampleHardware implements ExampleRequirments {
 
     @Override
     public void close() throws Exception {
         // TODO Auto-generated method stub
         throw new UnsupportedOperationException("Unimplemented method 'close'");
+    }
+
+    @Override
+    public void toLog(LogTable table) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'toLog'");
+    }
+
+    @Override
+    public void setLogName(String logName) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'setLogName'");
     }
 }
